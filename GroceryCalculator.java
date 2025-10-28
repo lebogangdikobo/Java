d78b6ac250da718ecc2e6edb73b8362bbde39420
lebogangdikobo <lebogangdikobo95@gmail.com>
2025-10-28 21:09:36 +0200

    done

--- GroceryCalculator.java
+++ GroceryCalculator.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class GroceryCalculator{
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
 
+        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Integer> quantities = new ArrayList<>();
+        ArrayList<Double> prices = new ArrayList<>();
+
+        System.out.println("===Groccery Calculator===");
+
+        System.out.print("Enter name of the item (or enter \'done\' to finish'): ");
+        String item = scan.nextLine();
+
+        while(!item.equalsIgnoreCase("done")){
+            System.out.print("Enter quantity for " + item + ": ");
+            int quantity = scan.nextInt();
+
+            System.out.print("Enter the price: R");
+            double price = scan.nextDouble();
+
+            items.add(item);
+            quantities.add(quantity);
+            prices.add(price);
+
+            System.out.print("Enter name of the item (or enter \'done\' to finish'): ");
+            item = scan.nextLine();
+        }
+
+        System.out.println("===Grocery Summary===");
+        double grandTotal = 0;
+
+        for(int i = 0; i < items.size(); i++){
+            double total = quantities.get(i) * prices.get(i);
+
+            System.out.println(items.get(i) + "\tQty: " + quantities.get(i) + "\tPrice: R" + prices.get(i) + "\tTotal: R" + total);
+
+            grandTotal += total;
+        }
+
+        System.out.println("\nGrand total: R" + grandTotal);
+
+        scan.close();
     }
 }
